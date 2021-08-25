6346c2138af2b4b22f9c4b47ea4dcaff71d29cf6
Joseph Shi <simpson.1025.1213@gmail.com>
2021-08-25 17:49:01 +0800

    basic version

---
+++ resources/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    
+    /*Mobile-nav*/
+    /*
+    $('.js--nav-icon').click(function(){
+          var nav = $('.js--main-nav');
+          var iocn =$('.js--nav-icon');
+
+          nav.slideToggle(200);
+          if (icon.hasClass('mobile-navi')){
+              icon.hide(0);
+          }
+          else{
+              icon.addNameitem('menu-outline');
+              icon.removeName('close-outline');
+          }
+
+    })
+    */
+   
+   const navSlide = () => {
+       const burger = document.querySelector('.burger');
+       const nav = document.querySelector ('.nav-links');
+       const navLinks = document.querySelectorAll('.nav-links li');
+
+       burger.addEventListener('click', () => {
+           nav.classList.toggle('nav-active');
+
+
+           navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.4}s`;
+    
+            }
+        });
+        burger.classList.toggle('toggle');
+
+       });
+
+       
+   }
+
+   
+    navSlide();
+
+});
