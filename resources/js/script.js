783fdbf14b7423aabbfc067ce3dcffd390576e2d
Joseph Shi <simpson.1025.1213@gmail.com>
2021-08-26 15:35:30 +0800

    ver3

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     /*Mobile-nav*/
     /*
     $('.js--nav-icon').click(function(){
@@ -17,33 +17,32 @@ $(document).ready(function() {
 
     })
     */
-   
-   const navSlide = () => {
-       const burger = document.querySelector('.burger');
-       const nav = document.querySelector ('.nav-links');
-       const navLinks = document.querySelectorAll('.nav-links li');
 
-       burger.addEventListener('click', () => {
-           nav.classList.toggle('nav-active');
+    const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+            console.log('ver3')
 
 
-           navLinks.forEach((link, index)=> {
-            if(link.style.animation){
-                link.style.animation = '';
-            }
-            else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.4}s`;
-    
-            }
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.4}s`;
+
+                }
+            });
+            burger.classList.toggle('toggle');
+
         });
-        burger.classList.toggle('toggle');
 
-       });
 
-       
-   }
+    }
 
-   
+
     navSlide();
 
 });
